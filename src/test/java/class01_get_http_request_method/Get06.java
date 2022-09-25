5a73f6af8670abf9c35eb45fe20f49906db65ea7
AykutSaglamaz <gulerrahim@gmail.com>
2022-09-25 07:05:34 -0400

    GET API

--- src/test/java/class01_get_http_request_method/Get06.java
+++ src/test/java/class01_get_http_request_method/Get06.java
@@ -2,11 +2,14 @@ package class01_get_http_request_method;
 
 import base_urls.HerOkuAppBaseUrl;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.Test;
+import org.testng.asserts.SoftAssert;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertEquals;
 
 public class Get06 extends HerOkuAppBaseUrl {
         /*
@@ -57,6 +60,31 @@ public class Get06 extends HerOkuAppBaseUrl {
                 body("bookingdates.checkout", equalTo("2019-01-01")).
                 body("additionalneeds", equalTo("Breakfast"));
 
+        //2.yol, JsonPath kullanarak. JsonPath bir class'tir ve JSON data'nin icerisinde hareket etmek icin bircok kullanisli methodu vardir.
+        response.
+                then().
+                assertThat().
+                statusCode(200).
+                contentType(ContentType.JSON);
+
+        // JsonPath objesini response objesinden olusturma
+        JsonPath json = response.jsonPath();
+        assertEquals("Dane", json.getString("firstname"));
+        assertEquals("Soyisim eslesmiyor", "Dominguez", json.getString("lastname"));
+        assertEquals("Totalprice eslesmiyor", 111, json.getInt("totalprice"));
+        assertEquals("Checkin date eslesmiyor", "2018-01-01", json.getString("bookingdates.checkin"));
+
+    //3.yol,
+        // i) SoftAssert objesi olusturarak
+        SoftAssert softAssert = new SoftAssert();
+
+        // ii) SoftAssert objesini kullanarak Assertion yap
+        softAssert.assertEquals(json.getString("firstname"), "Dane", "Isim eslesmiyor");
+        softAssert.assertEquals(json.getString("lastname"), "Dominguez", "Soyisim eslesmiyor");
+        softAssert.assertEquals(json.getInt("totalprice"), 111, "Totalprice eslesmiyor");
+
+        // MUTLAKA EN SONDA assertAll() yapilmali. Eger assertAll() kullanmazsaniz herzaman test gecti gorunur fakat bu anlamli olmayabilir
+        softAssert.assertAll();
 
     }
 }
