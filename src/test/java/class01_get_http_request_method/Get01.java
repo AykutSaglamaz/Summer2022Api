417f5216ee544d09ac81ed23151c662e6a93601a
AykutSaglamaz <gulerrahim@gmail.com>
2022-10-28 16:24:19 -0400

    get

--- src/test/java/class01_get_http_request_method/Get01.java
+++ src/test/java/class01_get_http_request_method/Get01.java
@@ -18,33 +18,33 @@ public class Get01 {
         And : Coklu 'Given, When, Then' icin kullanilir
      */
 
-     /* test case olusturma
-        Given
-            https://restful-booker.herokuapp.com/booking/3
-        When
-            Kullanici GET Request'i Url'e (APi) gonderir
-            User send a GET Request to the url (API)
-        Then
-             HTTP Statu Kodu 200 olmali
-            HTTP Status Code should be 200
-        And
-            Content Type'i JSON olmali
-            Content Type should be JSON
-        And
-            Statu Line(duzeyi) HTTP/1.1 200 OK olmali
-            Status Line should be HTTP/1.1 200 OK
-     */
+    /* test case olusturma
+       Given
+           https://restful-booker.herokuapp.com/booking/3
+       When
+           Kullanici GET Request'i Url'e (APi) gonderir
+           User send a GET Request to the url (API)
+       Then
+            HTTP Statu Kodu 200 olmali
+           HTTP Status Code should be 200
+       And
+           Content Type'i JSON olmali
+           Content Type should be JSON
+       And
+           Statu Line(duzeyi) HTTP/1.1 200 OK olmali
+           Status Line should be HTTP/1.1 200 OK
+    */
     @Test
-    public void get01(){
+    public void get01() {
         // 1) set Url
         String url = "https://restful-booker.herokuapp.com/booking/6";
 
         // 2) Set the expected data (beklenen data)
 
         // 3) GET Request gondermek ve GET Response almak icin Atomasyon kodunu yaz
-       Response response = given().when().get(url);// get() getter gibi datayi okuyacak
-                                            // get() kullandigimizda datayi okur ve alir
-       response.prettyPrint(); //prettyPrint() consolde yazdirmak icin kullanilir
+        Response response = given().when().get(url);// get() getter gibi datayi okuyacak
+        // get() kullandigimizda datayi okur ve alir
+        response.prettyPrint(); //prettyPrint() consolde yazdirmak icin kullanilir
 
         // 4) Assertion yap
     /*
@@ -56,7 +56,7 @@ public class Get01 {
      */
         response.then().assertThat().statusCode(200).contentType("application/JSON").statusLine("HTTP/1.1 200 OK");
 
-    // statusCode, contentType ve StatusLine gibi console yazdirma
+        // statusCode, contentType ve StatusLine gibi console yazdirma
         System.out.println(response.getStatusCode());
         System.out.println(response.getContentType());
         System.out.println(response.getStatusLine());
