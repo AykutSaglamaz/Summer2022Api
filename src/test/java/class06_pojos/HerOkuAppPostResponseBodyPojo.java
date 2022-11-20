b98151c2231325c291264ab2ad1f0f54dd1101e8
AykutSaglamaz <gulerrahim@gmail.com>
2022-11-20 15:35:16 -0500

    "update"

--- src/test/java/class06_pojos/HerOkuAppPostResponseBodyPojo.java
+++ src/test/java/class06_pojos/HerOkuAppPostResponseBodyPojo.java
@@ -1,5 +1,8 @@
 package class06_pojos;
 
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class HerOkuAppPostResponseBodyPojo {
     private Integer bookingid;
     private BookingPojo booking;
